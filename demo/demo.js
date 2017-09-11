a68d194217d2a700e255c79ad799e8a4aa71425b
Maksim Ovcharik <ovcharik@selectel.ru>
2017-09-11 15:06:29 +0300

    Editable steps and additional step options

--- demo/demo.js
+++ demo/demo.js
@@ -22,7 +22,7 @@ var StepperDemoController = (function () {
                 _this.campaign = !_this.campaign;
                 steppers.error('Wrong campaign');
             }
-        }, 3000);
+        }, 700);
     };
     StepperDemoController.prototype.previousStep = function () {
         var steppers = this.$mdStepper('stepper-demo');
@@ -64,6 +64,9 @@ var StepperDemoController = (function () {
         var steppers = this.$mdStepper('stepper-demo');
         steppers.clearFeedback();
     };
+    StepperDemoController.prototype.log = function(message) {
+        console.log(message);
+    };
     StepperDemoController.$inject = [
         '$mdStepper',
         '$timeout'
@@ -76,4 +79,3 @@ angular.module('SteppersApp', [
     'ngMaterial',
     'mdSteppers'
 ]).controller('SteppersDemoCtrl', StepperDemoController);
-
